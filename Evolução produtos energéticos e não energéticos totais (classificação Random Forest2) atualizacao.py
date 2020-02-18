547829085153e59212b5a003a382bbc82194856d
Aristóteles Fernandes <fernandessfae@hotmail.com>
2020-02-18 12:55:12 -0300

    Classificação Random Forest

---
+++ Evolução produtos energéticos e não energéticos totais (classificação Random Forest2) atualizacao.py
@@ -0,0 +1,46 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import confusion_matrix, accuracy_score
+from yellowbrick.classifier import ConfusionMatrix
+from sklearn.ensemble import RandomForestClassifier
+
+'''Aqui vamos usar um método de classificação usando a Floresta Randômica,
+para saber como o computador irá dizer qual o tipo de derivado do petróleo cada produto pertence. '''
+
+dados = pd.read_csv('Anuário Estatístico 2019 - Distribuição percentual da produção de derivados de petróleo não energéticos.csv', sep = ';', decimal = ',')
+
+#Criação de uma variável com variáveis independentes(x)
+previsores = dados.iloc[:, [1, 3]].values
+
+#Criação de uma variável com variável de resposta(y)
+classe = dados.iloc[:, 0].values
+
+#Aqui iremos transformar as colunas categóricas em colunas numéricas
+labelencoder = LabelEncoder()
+previsores[:, 0] = labelencoder.fit_transform(previsores[:, 0])
+
+#Aqui hávera a divisão dos dados para treinamento e teste 
+X_treinamento, X_teste, y_treinamento, y_teste = train_test_split(previsores, classe, test_size = 0.3, random_state = 0)
+
+#Criação do algoritmo da floresta randômica (Random Forest), juntamente com o treinamento do algoritmo
+floresta = RandomForestClassifier(criterion = 'gini', random_state = 0)
+floresta.fit(X_treinamento, y_treinamento)
+
+#Faz as previsões da variável teste.
+previsoes = floresta.predict(X_teste)
+
+#Gera uma variável com a matriz de confusão
+confusao = confusion_matrix(y_teste, previsoes)
+
+#Gera duas variáveis com as taxas de acertos e erros da floresta randômica (Random Forest)
+taxa_acerto = accuracy_score(y_teste, previsoes)
+taxa_erro = 1 - taxa_acerto
+
+#Gera o imagem da matriz de confusão
+v = ConfusionMatrix(floresta)
+v.fit(X_treinamento, y_treinamento)
+v.score(X_teste, y_teste)
+v.poof()
+
+'''Obs: Como constatado, a taxa de acerto foi 100%, com 30% de dados para teste.''' 
