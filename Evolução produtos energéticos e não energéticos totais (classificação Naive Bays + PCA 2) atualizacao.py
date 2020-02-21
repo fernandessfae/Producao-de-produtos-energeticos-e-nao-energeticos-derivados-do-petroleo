b4ed7592dc0b1edad0609121b328a67af56115a1
Aristóteles Fernandes <fernandessfae@hotmail.com>
2020-02-21 18:06:20 -0300

    Naive bays com redução dimensionalidade
    Utilização do PCA e KernelPCA para o modelo de classificação Naive Bays

---
+++ Evolução produtos energéticos e não energéticos totais (classificação Naive Bays + PCA 2) atualizacao.py
@@ -0,0 +1,58 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB
+from sklearn.preprocessing import LabelEncoder, StandardScaler
+from sklearn.metrics import confusion_matrix, accuracy_score
+from yellowbrick.classifier import ConfusionMatrix
+from sklearn.decomposition import PCA
+
+'''Aqui vamos usar um método de classificação usando o Naive Bays, para saber como o computador irá dizer qual o tipo de derivado do petróleo cada produto pertence. '''
+
+dados = pd.read_csv('Anuário Estatístico 2019 - Distribuição percentual da produção de derivados de petróleo não energéticos.csv', sep = ';', decimal = ',')
+
+#Criação de uma variável com variáveis independentes(x)
+previsores = dados.iloc[:, [1, 3]].values
+
+#Criação de uma variável com variável de resposta(y)
+classe = dados.iloc[:, 0].values
+
+#Aqui iremos transformar as colunas categóricas em colunas numéricas
+labelencoder = LabelEncoder()
+previsores[:, 0] = labelencoder.fit_transform(previsores[:, 0])
+
+#Fazendo a padronização dos atributos previsores
+scaler = StandardScaler()
+previsores = scaler.fit_transform(previsores)
+
+#Aqui hávera a divisão dos dados para treinamento e teste passando como parâmetros(variavel independente, variável resposta, a amostra de teste[0 até 1] e divisao da base de dados igual)
+X_treinamento, X_teste, y_treinamento, y_teste = train_test_split(previsores, classe, test_size = 0.3, random_state = 0)
+
+#Aplicação do PCA, que tem a função de reduzir a dimensionalidade dos atributos
+pca = PCA(n_components = 1)
+X_treinamento = pca.fit_transform(X_treinamento)
+X_teste = pca.transform(X_teste)
+componentes = pca.explained_variance_ratio_
+
+#Agora vamos aplicar o naive bays nos dados de treinamento (executa os dois comandos simultaneamente)
+naive_bayes = GaussianNB()
+naive_bayes.fit(X_treinamento, y_treinamento)
+
+#Pega os dados passados no passo anterior para ser executado no modelo
+previsoes = naive_bayes.predict(X_teste)
+
+#gera uma variável com uma matriz de confusão
+confusao = confusion_matrix(y_teste, previsoes)
+
+#Revela o percentual de acerto e erro do modelo da máquina
+taxa_acerto = accuracy_score(y_teste, previsoes)
+taxa_erro = 1 - taxa_acerto
+
+#Aqui irá gerá a figura da matriz de confusão (executar os 4 comandos simultaneamente)
+v = ConfusionMatrix(naive_bayes)
+v.fit(X_treinamento, y_treinamento)
+v.score(X_teste, y_teste)
+v.poof()
+
+'''Obs: Como constatado, a taxa de acerto do modelo foi 64.4%, aproximadamente, com 30% de dados para teste.
+        Com o PCA aplicado, a taxa de acerto do modelo foi 51.1%, aproximadamente, com 30% de dados para teste,
+        mas o seu processamento foi mais rápido.'''
